de68f4e075fd70ba6768621c7034bf0dc89c1317
TacioAntonio <tacioantonio10@gmail.com>
2023-09-28 11:36:46 -0300

    refactor: removed comments in MainActivity

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -67,44 +67,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import java.util.Objects
 
-//data class PokemonResponse(
-//    val id: Int,
-//    val name: String,
-//    val sprites: Any
-//    // Outros campos que você deseja mapear
-//)
-//
-//interface PokeApiService {
-//    @GET("pokemon/{name}")
-//    fun getPokemon(@Path("name") name: String): Call<PokemonResponse>
-//}
-//
-//fun takePokemon() {
-//    val retrofit = Retrofit.Builder()
-//        .baseUrl("https://pokeapi.co/api/v2/")
-//        .addConverterFactory(GsonConverterFactory.create())
-//        .build()
-//
-//    val pokeApiService = retrofit.create(PokeApiService::class.java)
-//    val call = pokeApiService.getPokemon("ditto")
-//
-//    call.enqueue(object : Callback<PokemonResponse> {
-//        override fun onResponse(call: Call<PokemonResponse>, response: Response<PokemonResponse>) {
-//            if (response.isSuccessful) {
-//                val pokemon = response.body()
-//                 println("Pokémon: "..pokemon.toString())
-//                // Faça algo com os dados do Pokémon aqui
-//            } else {
-//                // Lidar com erros aqui
-//            }
-//        }
-//
-//        override fun onFailure(call: Call<PokemonResponse>, t: Throwable) {
-//            // Lidar com falhas na comunicação aqui
-//        }
-//    })
-//}
-
 @Composable
 fun SplashScreen() {
     Box(
@@ -124,7 +86,6 @@ fun SplashScreen() {
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -140,134 +101,5 @@ class MainActivity : ComponentActivity() {
                 SplashScreen()
             }
         }
-//
-//        setContent {
-//            ApplicationTheme {
-//                // A surface container using the 'background' color from the theme
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .background(color = Color(0xFFD0BCFF))
-//                    //                    color = Color(0xFFD0BCFF)
-//                ) {
-//                    var currentName by remember { mutableStateOf("") };
-//                    var nameInput by remember { mutableStateOf("") }
-//
-//                    Text(
-//                        currentName.uppercase(),
-//                        fontWeight = FontWeight.Bold
-//                    );
-//
-//                    Row {
-//                        TextField(
-//                            value = nameInput,
-//                            onValueChange = {
-//                                nameInput = it
-//                            },
-//                            label = { Text("Label") }
-//                        )
-//
-//                        Button(
-//                            onClick = { currentName = nameInput },
-//                            modifier = Modifier
-//                                .padding(4.dp)
-//                                .wrapContentSize(Alignment.Center)
-//                                .height(IntrinsicSize.Min),
-//                            shape = CutCornerShape(10)
-//                        ) {
-//                            Text(
-//                                "Search".uppercase(),
-//                                fontSize = 16.sp,
-//                                fontWeight = FontWeight.Bold
-//                            );
-//                        }
-//                    }
-//
-//                    //                    takePokemon();
-//                    Column(
-//                        modifier   = Modifier.fillMaxSize(),
-//                        verticalArrangement = Arrangement.Center,
-//                        horizontalAlignment = Alignment.CenterHorizontally
-//                    ) {
-//                        AsyncImage(
-//                            model = "https://cdn-icons-png.flaticon.com/512/4128/4128240.png",
-//                            contentDescription = null,
-//                            modifier = Modifier.size(width = 64.dp, height = 64.dp)
-//                        );
-//
-//                        Spacer(modifier = Modifier.height(16.dp));
-//
-//                        Text(
-//                            "PokemonName".uppercase(),
-//                            fontWeight = FontWeight.Bold
-//                        );
-//
-//                        Spacer(modifier = Modifier.height(8.dp));
-//
-//                        Text("Type 1");
-//                        Text("Type N");
-//                    }
-//                }
-//            }
-//        }
     }
-
 }
-
-//val peoples: List<People> = listOf(People(
-//    "Max",
-//    "https://cdn-icons-png.flaticon.com/512/4128/4128240.png"
-//), People(
-//    "Octavio",
-//    "https://cdn-icons-png.flaticon.com/512/9308/9308310.png"
-//));
-
-//peoples.forEach { eachPeople ->
-//    Row {
-//        AsyncImage(
-//            model = eachPeople.image,
-//            contentDescription = null,
-//            modifier = Modifier.size(width = 64.dp, height = 64.dp)
-//        )
-//
-//        Spacer(modifier = Modifier.width(16.dp))
-//
-//        Column {
-//            Text(
-//                eachPeople.name,
-//                fontWeight = FontWeight.Bold
-//            );
-//            Text("Description");
-//        }
-//    }
-//}
-class People(name: String = "Sem nome", image: String = "") {
-   val name: String = name;
-    val image: String = image;
-}
-
-@Composable
-fun Greeting(people: People, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi ${people.name.uppercase()}! Welcome!",
-        modifier = modifier,
-        style = TextStyle(
-//            color = Color(255, 255, 255),
-            fontWeight = FontWeight.Bold,
-            fontSize = 22.sp
-        )
-    )
-
-}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ApplicationTheme {
-//        Greeting(People("Tácio"))
-//    }
-//}
-
-//implementation("io.coil-kt:coil:2.4.0")
-//implementation("io.coil-kt:coil-compose:2.4.0")
-//<uses-permission android:name="android.permission.INTERNET" />
